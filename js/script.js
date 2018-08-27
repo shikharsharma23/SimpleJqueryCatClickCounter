3f221c90525cdf81c29b0a30a6ad846b596a5e7f
Shikhar Sharma <shikharci23@gmail.com>
2018-08-27 21:22:30 +0530

    Adding 2 cats clicker

--- js/script.js
+++ js/script.js
@@ -1,6 +1,13 @@
-$('#catImg').click(function(e) {
-  var count = parseInt($('#count')[0].innerHTML); // access first eleemnt of jquery or use document.getElementById
-  count = count+1;
-  $('#count').text(count);
-  console.log(count);
+$('#catImg1').click(function(e) {
+  var count1 = parseInt($('#count1')[0].innerHTML); // access first eleemnt of jquery or use document.getElementById
+  count1 = count1+1;
+  $('#count1').text(count1);
+  console.log(count1);
+});
+
+$('#catImg2').click(function(e) {
+  var count2 = parseInt($('#count2')[0].innerHTML); // access first eleemnt of jquery or use document.getElementById
+  count2 = count2+1;
+  $('#count2').text(count2);
+  console.log(count2);
 });
